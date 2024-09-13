81a002a181f936ac7d8a80190266050b7e4d657a
isjxn <jani123123123.diederich@gmx.de>
2024-09-13 21:39:27 +0200

    Add firebase options

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:tagfalter_monitoring/firebase_options.dart';
 
 import 'menupage.dart';
 import 'datapage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const NavigationBarApp());
 }
 
