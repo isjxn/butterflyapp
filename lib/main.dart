0cbc9de26516039ea2f4605abc5a6426847aae9f
Andrea Pérez Ibáñez <andreapeib@gmail.com>
2024-09-13 10:32:07 +0200

    Some modifcations added (home page text, white icons, and fake filter)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -32,84 +33,118 @@ class NavigationExample extends StatefulWidget {
 
 class _NavigationExampleState extends State<NavigationExample> {
   int currentPageIndex = 0;
+  final TextEditingController _searchController = TextEditingController();
+  String _searchText = "";
+
+  // Firestore reference to the butterflies collection
+  final CollectionReference _butterfliesRef =
+  FirebaseFirestore.instance.collection('butterflies');
+
+  List<String> _filteredButterflyList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _searchController.addListener(_onSearchChanged);
+  }
+
+  // Function to handle search logic
+  void _onSearchChanged() async {
+    setState(() {
+      _searchText = _searchController.text.toLowerCase();
+    });
+
+    if (_searchText.isNotEmpty) {
+      QuerySnapshot querySnapshot = await _butterfliesRef
+          .where('name', isGreaterThanOrEqualTo: _searchText)
+          .where('name', isLessThanOrEqualTo: '$_searchText\uf8ff')
+          .get();
+
+      setState(() {
+        _filteredButterflyList = querySnapshot.docs
+            .map((doc) => doc['name'] as String)
+            .toList();
+      });
+    } else {
+      setState(() {
+        _filteredButterflyList = [];
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
     return Scaffold(
-      extendBody: true, // Ensures FAB extends over the bottom app bar
+      extendBody: true,
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Add your functionality here
-        },
-        backgroundColor: const Color.fromARGB(255, 25, 98, 2), // Change button background color
-        foregroundColor: Colors.black, 
+        onPressed: () {},
+        backgroundColor: const Color.fromARGB(255, 25, 98, 2),
+        foregroundColor: Colors.black,
         shape: const CircleBorder(),
         child: const Icon(Icons.camera_alt),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      
-      // BottomAppBar with custom buttons
+
       bottomNavigationBar: BottomAppBar(
         padding: const EdgeInsets.symmetric(horizontal: 10),
         height: 60,
         color: const Color.fromARGB(255, 119, 171, 105),
-        shape: const CircularNotchedRectangle(), // Allows FAB to notch into BottomAppBar
+        shape: const CircularNotchedRectangle(),
         notchMargin: 5,
         child: Row(
           mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
             IconButton(
-              icon: const Icon(
+              icon: Icon(
                 Icons.home,
-                color: Colors.black,
+                color: currentPageIndex == 0 ? Colors.white : Colors.black,
               ),
               onPressed: () {
                 setState(() {
-                  currentPageIndex = 0; // Navigate to the "Übersichts" page
+                  currentPageIndex = 0;
                 });
               },
             ),
             IconButton(
-              icon: const Icon(
+              icon: Icon(
                 Icons.search,
-                color: Colors.black,
+                color: currentPageIndex == 1 ? Colors.white : Colors.black,
               ),
               onPressed: () {
                 setState(() {
-                  currentPageIndex = 1; // Navigate to the "Bericht" page
+                  currentPageIndex = 1;
                 });
               },
             ),
             IconButton(
-              icon: const ImageIcon(
-                AssetImage('assets/images/schmetterling.png'),  // Path to your custom icon
-                size: 24.0,  // Adjust the size as needed
-                color: Colors.black,
-              ),
-            onPressed: () {
-              setState(() {
-                currentPageIndex = 3; // Navigate to the "Data" page
-              });
-            },
-          ),
-            IconButton(
-              icon: const Icon(
-                Icons.menu,
-                color: Colors.black,
+              icon: ImageIcon(
+                const AssetImage('assets/images/schmetterling.png'),
+                size: 24.0,
+                color: currentPageIndex == 2 ? Colors.white : Colors.black,
               ),
               onPressed: () {
                 setState(() {
-                currentPageIndex = 4; // Navigate to the "Menu" page
-              });
+                  currentPageIndex = 2;
+                });
+              },
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.menu,
+                color: currentPageIndex == 3 ? Colors.white : Colors.black,
+              ),
+              onPressed: () {
+                setState(() {
+                  currentPageIndex = 3;
+                });
               },
             ),
           ],
         ),
       ),
-      
-      
+
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 119, 171, 105),
         foregroundColor: Colors.black,
@@ -117,61 +152,128 @@ class _NavigationExampleState extends State<NavigationExample> {
         title: Row(
           children: [
             Image.asset(
-              'assets/images/schmetterling.png', // Replace with your actual logo
+              'assets/images/schmetterling.png',
               height: 40.0,
             ),
             const SizedBox(width: 10),
-            const Text('Tagfalter Monitoring', style: TextStyle(color: Colors.black)),
+            const Text('Tagfalter Monitoring',
+                style: TextStyle(color: Colors.black)),
           ],
         ),
       ),
       // Body content changes based on current page index
       body: <Widget>[
         // Home page
-        Card(
-          shadowColor: Colors.transparent,
-          margin: const EdgeInsets.all(8.0),
-          child: SizedBox.expand(
-            child: Center(
-              child: Text(
-                'Home Page',
-                style: theme.textTheme.titleLarge,
+        Scrollbar(
+          thumbVisibility: true,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Willkommen zum Tagfalter Monitoring',
+                    style: theme.textTheme.headlineSmall
+                        ?.copyWith(fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    'TMD zur neuen ONLINE-DATENEINGABE (ab 2024)',
+                    style: theme.textTheme.titleMedium,
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    'Im Frühjahr 2005 startete das Tagfalter-Monitoring in Deutschland. '
+                        'Jahr für Jahr erfassen Freiwillige bei wöchentlichen Begehungen entlang '
+                        'festgelegter Strecken alle tagaktiven Schmetterlinge. Die gesammelten Daten '
+                        'dokumentieren die Entwicklung der Schmetterlingsbestände auf lokaler, '
+                        'regionaler und nationaler Ebene und können mit denen anderer europäischer Länder '
+                        'verglichen werden, in denen diese Beobachtungen schon seit Jahrzehnten durchgeführt werden.',
+                    style: theme.textTheme.bodyLarge,
+                    textAlign: TextAlign.justify,
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    'Das Projekt wird von zahlreichen Organisationen unterstützt, darunter NABU, BUND, '
+                        'das Bundesamt für Naturschutz (BfN), entomologische Verbände, die Gesellschaft für '
+                        'Schmetterlingsschutz sowie die europäische Stiftung Butterfly Conservation Europe.',
+                    style: theme.textTheme.bodyLarge,
+                    textAlign: TextAlign.justify,
+                  ),
+                  const SizedBox(height: 8), // Reduced space between text and image
+                  Center(
+                    child: Image.asset(
+                      'assets/images/UFZ_Logo.png',
+                      height: 150,
+                    ),
+                  ),
+                  const SizedBox(height: 30), // Added space after the logo
+                ],
               ),
             ),
           ),
         ),
-        // Search page, search via traits -> boolean
-        Card(
-          shadowColor: Colors.transparent,
-          margin: const EdgeInsets.all(8.0),
-          child: SizedBox.expand(
-            child: Center(
-              child: Text(
-                'Search Page',
-                style: theme.textTheme.titleLarge,
+        // Search page with search bar and list of butterflies
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              // Search bar
+              TextField(
+                controller: _searchController,
+                decoration: InputDecoration(
+                  labelText: 'Suchen schmetterling...',
+                  prefixIcon: const Icon(Icons.search),
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                    borderSide: BorderSide(
+                      color: const Color.fromARGB(255, 119, 171, 105),
+                      width: 2.0,
+                    ),
+                  ),
+                ),
               ),
-            ),
+              const SizedBox(height: 20),
+              // Display filtered list of butterflies from Firebase
+              Expanded(
+                child: _filteredButterflyList.isEmpty
+                    ? Center(
+                  child: Text(
+                    'Schmetterling nicht gefunden',
+                    style: theme.textTheme.bodyLarge,
+                  ),
+                )
+                    : ListView.builder(
+                  itemCount: _filteredButterflyList.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(_filteredButterflyList[index]),
+                      onTap: () {
+                        // Add functionality to navigate to a detailed page or action if needed
+                      },
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
         ),
-        // todo: Camera integation here
-        Card(
-          shadowColor: Colors.transparent,
-          margin: const EdgeInsets.all(8.0),
-          child: SizedBox.expand(
-            child: Center(
-              child: Text(
-                'Add Image Page',
-                style: theme.textTheme.titleLarge,
-              ),
-            ),
+        // Add Image page
+        Center(
+          child: Text(
+            'Bild hinzufügen',
+            style: theme.textTheme.titleLarge,
           ),
         ),
-        // DataPage (now coming from datapage.dart)
-        const DataPage(),  
-        // MenuPage (from menupage.dart)
-        const MenuPage(), // Load MenuPage
+        // Data page
+        const DataPage(),
+        // Menu page
+        const MenuPage(),
       ][currentPageIndex],
     );
   }
 }
-
